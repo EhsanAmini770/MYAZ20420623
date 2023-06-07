f6007e0002ec38c7560b3e0ab9faca1d1817075a
EhsanAmini770 <u201118076@samsun.edu.tr>
2023-06-07 14:15:28 +0300

    Tree and Test for Tree added

---
+++ Data Structures/Apps/DataStructureApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Data.SQLite;
+using SortingAlgorithms;
+
+string filePath = "Employee.db";
+string connectionString = $"Data Source={filePath};Version=3;";
+
+
+
+
+
+// Establish a connection to the database
+using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+{
+    connection.Open();
+
+    string query = "SELECT * FROM Employees;";
+
+    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+    {
+        using (SQLiteDataReader reader = command.ExecuteReader())
+        {
+            // Load the data into a list
+            List<Employee> employees = new List<Employee>();
+            while (reader.Read())
+            {
+                int id = Convert.ToInt32(reader["Id"]);
+                string firstName = reader["FirstName"].ToString();
+                string lastName = reader["LastName"].ToString();
+                decimal salary = Convert.ToDecimal(reader["Salary"]);
+                string title = reader["Title"].ToString();
+
+                employees.Add(new Employee(id, firstName, lastName, salary, title));
+            }
+
+            // Display menu for sorting options
+
+            Console.WriteLine("2. Insertion Sort");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the number corresponding to the sorting algorithm:");
+            int algorithmChoice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the field to sort by (1. Id, 2. First Name, 3. Salary):");
+            int fieldChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (fieldChoice)
+            {
+                case 1: // Sort by Id
+                    BubbleSort.Sort(employees.Select(e => e.FirstName).ToArray());
+                    break;
+
+                case 2: // Sort by First Name
+                    InsertionSort.Sort(employees.Select(e => e.FirstName).ToArray());
+
+                    break;
+                case 3: // Sort by Salary
+                    InsertionSort.Sort(employees.Select(e => e.Salary).ToArray());
+
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid field choice. Exiting the program.");
+                    return;
+            }
+        }
+    }
+    connection.Close();
+}
---
+++ Data Structures/Tests/BinaryTreeTests/BinaryTressTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trees.BinaryTree;
+
+namespace BinaryTreeTests
+{
+    public class BinaryTressTests
+    {
+        private BinaryTree<int> bt;
+        public BinaryTressTests()
+        {
+            // Arrange
+            bt = new BinaryTree<int>();
+        }
+
+        [Fact]
+        public void Insert_Create_Root()
+        {
+            // Act
+            bt.Insert(1);
+
+            // Assert
+            Assert.Equal(1, bt.Root.Value);
+        }
+
+        [Fact]
+        public void Insert_Check_Left_Node()
+        {
+            // Act
+            bt.Insert(1);
+            bt.Insert(2);
+
+            // Assert
+            Assert.Equal(1, bt.Root.Value);
+            Assert.Equal(2, bt.Root.Left.Value);
+        }
+
+        [Fact]
+        public void Insert_Check_Right_Node()
+        {
+            // Act
+            bt.Insert(1);
+            bt.Insert(2);
+            bt.Insert(3);
+
+            // Assert
+            Assert.Equal(1, bt.Root.Value);
+            Assert.Equal(2, bt.Root.Left.Value);
+            Assert.Equal(3, bt.Root.Right.Value);
+        }
+
+        [Fact]
+        public void Multiple_Insertion_Check()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7 }.ToList()
+                .ForEach(x => bt.Insert(x));
+
+
+            // Assert
+            Assert.Equal(1, bt.Root.Value);
+            Assert.Equal(2, bt.Root.Left.Value);
+            Assert.Equal(3, bt.Root.Right.Value);
+            Assert.Equal(4, bt.Root.Left.Left.Value);
+            Assert.Equal(5, bt.Root.Left.Right.Value);
+            Assert.Equal(6, bt.Root.Right.Left.Value);
+            Assert.Equal(7, bt.Root.Right.Right.Value);
+        }
+
+        [Fact]
+        public void Count_Check()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+            .Where(x => x % 2 == 0)
+            .ToList()
+            .ForEach(x => bt.Insert(x));
+
+            // Assert
+            Assert.Equal(2, bt.Root.Value);
+            Assert.Equal(4, bt.Root.Left.Value);
+            Assert.Equal(6, bt.Root.Right.Value);
+            Assert.Equal(8, bt.Root.Left.Left.Value);
+            Assert.Equal(10, bt.Root.Left.Right.Value);
+
+            Assert.Equal(5, bt.Count);
+        }
+
+
+        [Fact]
+        public void Level_Order_Traverse_Test()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            .ToList()
+            .ForEach(x => bt.Insert(x));
+
+            var list = BinaryTree<int>.LevelOrderTraverse(bt.Root);
+
+            // Assert
+            Assert.Collection(list,
+                item => Assert.Equal(bt.Root.Value, item.Value),
+                item => Assert.Equal(2, item.Value),
+                item => Assert.Equal(3, item.Value),
+                item => Assert.Equal(4, item.Value),
+                item => Assert.Equal(5, item.Value),
+                item => Assert.Equal(6, item.Value),
+                item => Assert.Equal(7, item.Value));
+
+            Assert.Equal(7, bt.Count);
+
+        }
+
+        [Fact]
+        public void GetEnumerator_Test()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            .ToList()
+            .ForEach(x => bt.Insert(x));
+
+            var list = BinaryTree<int>.LevelOrderTraverse(bt.Root);
+            // Assert
+            Assert.Collection(list,
+                item => Assert.Equal(bt.Root.Value, item.Value),
+                item => Assert.Equal(2, item.Value),
+                item => Assert.Equal(3, item.Value),
+                item => Assert.Equal(4, item.Value),
+                item => Assert.Equal(5, item.Value),
+                item => Assert.Equal(6, item.Value),
+                item => Assert.Equal(7, item.Value));
+
+            Assert.Equal(7, bt.Count);
+
+        }
+
+
+        [Fact]
+        public void IsLeaf_Test()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            .ToList()
+            .ForEach(x => bt.Insert(x));
+
+            // Assert
+            Assert.True(bt.Root.Left.Left.IsLeaf);
+
+        }
+
+        [Fact]
+        public void IsNotLeaf_Test()
+        {
+            // Act
+            new int[] { 1, 2, 3, 4, 5, 6, 7 }
+            .ToList()
+            .ForEach(x => bt.Insert(x));
+
+            // Assert
+            Assert.False(bt.Root.Right.IsLeaf);
+        }
+    }
+}
